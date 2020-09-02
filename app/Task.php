68399d965ff8bd50730f2869a7f9bd159d55bb7e
Thanh Nguyen <vnscriptkid@gmail.com>
2020-09-02 15:28:24 +0700

    In project list, display the most recent updated first

--- app/Task.php
+++ app/Task.php
@@ -8,6 +8,8 @@ class Task extends Model
 {
     protected $fillable = ['body', 'completed'];
 
+    protected $touches = ['project'];
+
     public function path() {
         return $this->project->path() . '/tasks/' . $this->id;
     }
