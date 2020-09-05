a06c4f47b329ea6174684e191a53501368fb9e4a
Thanh Nguyen <vnscriptkid@gmail.com>
2020-09-05 22:53:06 +0700

    Add vue modal component to add project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import vmodal from 'vue-js-modal'
+
+Vue.use(vmodal)
+
+new Vue({
+    el: '#app'
+});
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,38 @@
 @section('content')
     <div class="d-flex justify-content-between mb-3 align-items-center">
         <h1 class="h1">My Projects</h1>
-        <a href="/projects/create" class="btn btn-info">Add Project</a>
+        <div>
+            <a href="/projects/create" class="btn btn-info">Add Project</a>
+            <modal name="addNewProject">
+                <div class="p-3">
+                    <h2 class="text-center">Let's start a project</h2>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="projectTitle">Title</label>
+                                <input name="title" type="text" class="form-control" id="projectTitle" placeholder="Project title" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="projectDescription">Description</label>
+                                <textarea name="description" id="projectDescription" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label>Need some tasks?</label>
+                                <input name="task" type="text" class="form-control" placeholder="A task" required>
+                            </div>
+                            <button class="btn">+ One more task</button>
+                        </div>
+                    </div>
+                    <div class="text-right">
+                        <button class="btn btn-info">Cancel</button>
+                        <button class="btn btn-info">Create Project</button>
+                    </div>
+                </div>
+            </modal>
+            <a href="" @click.prevent="$modal.show('addNewProject')">Add Project</a>
+        </div>
     </div>
     {{-- project list --}}
     <div class="d-flex flex-wrap justify-content-between">
